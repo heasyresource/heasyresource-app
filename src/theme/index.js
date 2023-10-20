006b6f5bd42fe3a962619889ce89c674e103bb2b
devwalex <salamiusmanolawale@gmail.com>
2023-10-20 15:21:06 +0100

    fix: app structure

---
+++ src/theme/index.js
@@ -0,0 +1,9 @@
+"use client";
+
+import { createTheme } from "@mantine/core";
+
+export const theme = createTheme({
+  colorScheme: "light",
+  fontFamily: "Poppins, sans-serif",
+  headerFontFamily: "Mulish, sans-serif",
+});
