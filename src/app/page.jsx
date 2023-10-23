a55392df60129fb958af1bc14270371102fae9ec
alameen-alli <allialameen8739@gmail.com>
2023-10-23 14:42:59 +0100

    feat: design minicard component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,22 +1,34 @@
+
+import MiniCard from "@/components/MiniCard";
 import SecureUser from "@/layout";
-import { Box, Text } from "@mantine/core";
+import { Box, Flex, Text } from "@mantine/core";
 import Link from "next/link";
 
 export default function HomePage() {
-  return (
-    <SecureUser>
-      <Box
-        w={600}
-        mx="auto"
-        ta="center"
-        c="#fff"
-        size={"xs"}
-        bg="blue.7"
-        py="xl"
-      >
-        <Text tt="capitalize">welcome to Heasyresource</Text>
-        <Link href="/signin">Sign In</Link> |<Link href="/signup">Sign Up</Link>
-      </Box>
-    </SecureUser>
-  );
+    return (
+        // <SecureUser>
+        <Box
+            w={600}
+            mx="auto"
+            ta="center"
+            c="#fff"
+            size={"xs"}
+            bg="blue.7"
+            py="xl"
+        >
+            <Text tt="capitalize">welcome to Heasyresource</Text>
+            <Link href="/signin">Sign In</Link> |<Link href="/signup">Sign Up</Link>
+            <Flex
+                justify="center"
+                mt={32}
+            >
+            <MiniCard
+                width="179px"
+                title="Total Employees"
+                value={"6,789"}
+            />
+            </Flex>
+        </Box>
+        // </SecureUser>
+    );
 }
---
+++ src/components/MiniCard.jsx
@@ -0,0 +1,40 @@
+'use client'
+import React from 'react';
+import NextImage from 'next/image';
+import profileIcon from '../../public/profileicon.svg'
+import { Card, Center, Flex, Image, Text } from '@mantine/core';
+
+// Define your custom Card component
+function MiniCard({ width, height, borderColor, title, value }) {
+  return (
+    <Card
+      style={{ width, height, borderRadius: '15px', borderColor }}
+      px="28"
+      py="20"
+      withBorder
+      padding="xl"
+      bg="var(--mantine-color-body)"
+    >
+      <Text style={{ color: '#7EA6F4' }} fz="sm" fw={700}>
+        {title}
+      </Text>
+      <Flex
+        mt={8}
+        align="center"
+        justify="space-between"
+      >
+        <Text style={{fontSize: '24px'}} fw={700}>
+          {value}
+        </Text>
+        <Image
+          component={NextImage}
+          h={15}
+          w="auto"
+          fit="contain"
+          src={profileIcon} alt="hr-logo" />
+      </Flex>
+    </Card>
+  );
+}
+
+export default MiniCard;
