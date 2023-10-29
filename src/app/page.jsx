8ed51784e2a8bc0237708a951b4ede554cb63891
devwalex <salamiusmanolawale@gmail.com>
2023-10-29 17:48:12 +0100

    chore: remove bg overlay

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,19 @@
-import Layout from "@/components/Layout";
 import { Box, Text } from "@mantine/core";
+import Link from "next/link";
 
 export default function HomePage() {
   return (
-    // <SecureUser>
-    <Layout>
-      <Box>
-        <Text>Dashboard</Text>
-      </Box>
-    </Layout>
-    // </SecureUser>
+    <Box
+      w={600}
+      mx="auto"
+      ta="center"
+      c="#fff"
+      size={"xs"}
+      bg="blue.7"
+      py="xl"
+    >
+      <Text tt="capitalize">welcome to Heasyresource</Text>
+      <Link href="/signin">Sign In</Link> |<Link href="/signup">Sign Up</Link>
+    </Box>
   );
 }
