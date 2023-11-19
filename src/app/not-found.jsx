b317006d0a812dd28ef61eb380ad74374eec432b
devwalex <salamiusmanolawale@gmail.com>
2023-11-19 17:05:11 +0100

    chore: checking for subdomain

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -2,11 +2,25 @@ import Link from 'next/link'
 import { headers } from 'next/headers'
 import { getSubdomain } from '@/utils/publicFunctions';
 
-export default function NotFound() {
+export default async function NotFound() {
   const headersList = headers()
   const domain = headersList.get('host')
+  console.log({ domain });
   const subdomain = getSubdomain(domain);
-  const link = subdomain && subdomain !== 'heasyresource' ? process.env.NEXTAUTH_URL : '/';
+  console.log("NOR", { subdomain });
+  // const link = (subdomain && subdomain !== 'heasyresource') ? process.env.NEXTAUTH_URL : '/';
+  const defaultSubdomain = ['www', 'heasyresource']
+  let link = '/';
+  if (subdomain && !defaultSubdomain.includes(subdomain)) {
+    // link = `https://${domain}`
+    const getSubdomain = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}/companies/subdomain/${subdomain}`);
+    const getSubdomainData = await getSubdomain.json();
+    if (getSubdomainData.results !== null) {
+      link = `http://${domain}`
+    } else {
+      link = process.env.NEXTAUTH_URL
+    }
+  }
   return (
     <div>
       <h2>Not Found</h2>
